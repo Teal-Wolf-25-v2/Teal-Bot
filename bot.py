9a53a3301fc95318f5d8c6c9e9a272b3fb11d67f
Teal-Wolf-25-Altgit config --global user.name Teal-Wolf-25-Altgit config --global user.name Teal-Wolf-25-Alt <aj50p@outlook.com>
2025-10-18 17:28:29 -0400

    fixed commit link?

--- bot.py
+++ bot.py
@@ -25,19 +25,19 @@ bot_pfp = "https://github.com/Teal-Wolf-25-v2/Teal-Bot/blob/main/icon.png?raw=tr
 bot_commit = requests.get("https://api.github.com/repos/Teal-Wolf-25-v2/Teal-Bot/commits/main")
 if bot_commit.status_code == 200:
     bot_hash = bot_commit.json()["sha"]
-    bot_hash_short = "["+bot_hash[-7:]+"](https://repo.tw25.net/Teal-Bot/commit/"+bot_hash+")"
+    bot_hash_short = "[`"+bot_hash[-7:]+"`](https://repo.tw25.net/Teal-Bot/commit/"+bot_hash+")"
     bot_updated = bot_commit.json()["commit"]["author"]["date"]
     updated_unix = int(datetime.fromisoformat(bot_updated.replace("Z","+00:00")).timestamp())
 else:
     bot_hash = "Error: Not Found"
     bot_hash_short = "null"
-bot_version = "0.2.4"
+bot_version = "0.2.5"
 bot_uptime = int(datetime.now().timestamp())
 
 bot_info=discord.Embed(color=0x00ffff,title="Cyan",url="https://repo.tw25.net/Teal-Bot",description="Discord bot to handle various Teal Wolf 25's Nexus functions.")
 bot_info.set_author(name="Teal Wolf 25",url="https://repo.tw25.net",icon_url=owner_pfp)
 bot_info.set_thumbnail(url=bot_pfp)
-bot_info.add_field(name="Version",value=f"{bot_version} `{bot_hash_short}`",inline=False)
+bot_info.add_field(name="Version",value=f"{bot_version} {bot_hash_short}",inline=False)
 bot_info.add_field(name="Handles",value="- Bot Info\n- Maafia Voting\n  - Embed Message",inline=False)
 bot_info.add_field(name="Last Updated",value=f"<t:{updated_unix}:F> (<t:{updated_unix}:R>)")
 bot_info.add_field(name="Uptime",value=f"Since <t:{bot_uptime}:R>")
