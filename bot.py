75379583e18cf03e8a94e5847d07666689086297
Teal-Wolf-25-Altgit config --global user.name Teal-Wolf-25-Altgit config --global user.name Teal-Wolf-25-Alt <aj50p@outlook.com>
2025-10-18 19:04:44 -0400

    Switched Embed to only display emojis for people currently in the voice channel

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ if bot_commit.status_code == 200:
 else:
     bot_hash = "Error: Not Found"
     bot_hash_short = "null"
-bot_version = "0.2.5"
+bot_version = "0.3.0"
 bot_uptime = int(datetime.now().timestamp())
 
 bot_info=discord.Embed(color=0x00ffff,title="Cyan",url="https://repo.tw25.net/Teal-Bot",description="Discord bot to handle various Teal Wolf 25's Nexus functions.")
@@ -42,20 +42,23 @@ bot_info.add_field(name="Handles",value="- Bot Info\n- Maafia Voting\n  - Embed 
 bot_info.add_field(name="Last Updated",value=f"<t:{updated_unix}:F> (<t:{updated_unix}:R>)")
 bot_info.add_field(name="Uptime",value=f"Since <t:{bot_uptime}:R>")
 
-maafia_voting=discord.Embed(color=0xbb00ff,title="Maafia Voting",description="React with the emoji that matches the person you want to vote")
-with open("emojis.json","r") as file:
-    emoji_objects=json.load(file)
-maafia_invalids = ""
-for emoji_obj in emoji_objects:
-    emoji_holder = emoji_obj["name"]
-    if emoji_obj["invalid"] == True:
-        emoji = "Invalid Emoji!"
-        maafia_invalids = maafia_invalids+"<@"+emoji_obj["user_id"]+">"
-    else:
-        emoji = "<"+emoji_obj["emoji_name"]+emoji_obj["emoji_id"]+">"
-    maafia_voting.add_field(name=emoji_holder,value=emoji,inline=False)
-maafia_invalids=maafia_invalids+" Please send Teal a valid emoji to use"
-maafia_voting.add_field(name="Skip",value="<:not_mafia:1281781263937573038>",inline=False)
+def maafia_voting_embed(vc_list):
+    maafia_voting=discord.Embed(color=0xbb00ff,title="Maafia Voting",description="React with the emoji that matches the person you want to vote")
+    with open("emojis.json","r") as file:
+        emoji_objects=json.load(file)
+    maafia_invalids = ""
+    for emoji_obj in emoji_objects:
+        if emoji_obj["user_id"] in vc_list:
+            emoji_holder = emoji_obj["name"]
+            if emoji_obj["invalid"] == True:
+                emoji = "Invalid Emoji!"
+                maafia_invalids = maafia_invalids+"<@"+emoji_obj["user_id"]+">"
+            else:
+                emoji = "<"+emoji_obj["emoji_name"]+emoji_obj["emoji_id"]+">"
+            maafia_voting.add_field(name=emoji_holder,value=emoji,inline=False)
+        maafia_invalids=maafia_invalids+" Please send Teal a valid emoji to use"
+        maafia_voting.add_field(name="Skip",value="<:not_mafia:1281781263937573038>",inline=False)
+    return maafia_voting,maafia_invalids
 
 @tree.command(
     name="info",
@@ -68,6 +71,14 @@ async def info(interaction):
     description="Prompts the Maafia Voting embed message"
 )
 async def maafia(interaction):
+    maafia_channel_id = 123456789012345678
+    maafia_channel = interaction.guild.get_channel(maafia_channel_id)
+    if maafia_channel and isinstance(maafia_channel, discord.VoiceChannel):
+        member_ids = [member.id for member in maafia_channel.members]
+    maafia_member_ids=[]
+    for member in member_ids:
+        maafia_member_ids=maafia_member_ids.append(str(member))
+    maafia_voting,maafia_invalids=maafia_voting_embed(maafia_member_ids)
     await interaction.response.send_message(content=maafia_invalids,embed=maafia_voting,allowed_mentions=discord.AllowedMentions(everyone=True,users=True))
 
 commands = [com for com in tree.walk_commands() if isinstance(com, app_commands.Command)]
